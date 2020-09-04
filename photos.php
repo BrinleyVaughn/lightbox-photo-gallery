da628f52b21329f7a7e5a3f50aab7a527a74ac86
Brinley <49964281+BrinleyVaughn@users.noreply.github.com>
2020-09-04 03:15:09 +0200

    Update photos.php

--- photos.php
+++ photos.php
@@ -3,74 +3,74 @@
 function myPhotos() {
     $photos = [
         [
-            'url' => 'http://lorempixel.com/1280/560/transport/1',
-            'thumbnail' => 'http://lorempixel.com/500/500/transport/1',
+            'url' => 'https://lorempixel.com/1280/560/transport/1',
+            'thumbnail' => 'https://lorempixel.com/500/500/transport/1',
             'caption' => 'Photo 1'
         ],
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/2',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/2',
+            'url' => 'https://lorempixel.com/1280/560/nature/2',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/2',
             'caption' => 'Photo 2'
         ],
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/3',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/3',
+            'url' => 'https://lorempixel.com/1280/560/nature/3',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/3',
             'caption' => 'Photo 3'
         ],
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/4',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/4',
+            'url' => 'https://lorempixel.com/1280/560/nature/4',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/4',
             'caption' => 'Photo 4'
         ],
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/5',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/5',
+            'url' => 'https://lorempixel.com/1280/560/nature/5',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/5',
             'caption' => 'Photo 5'
         ],
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/6',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/6',
+            'url' => 'https://lorempixel.com/1280/560/nature/6',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/6',
             'caption' => 'Photo 6'
         ],
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/7',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/7',
+            'url' => 'https://lorempixel.com/1280/560/nature/7',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/7',
             'caption' => 'Photo 7'
         ],
         
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/8',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/8',
+            'url' => 'https://lorempixel.com/1280/560/nature/8',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/8',
             'caption' => 'Photo 8'
         ],
         
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/1',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/1',
+            'url' => 'https://lorempixel.com/1280/560/nature/1',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/1',
             'caption' => 'Photo 9'
         ],
         
         [
-            'url' => 'http://lorempixel.com/1280/560/nature/10',
-            'thumbnail' => 'http://lorempixel.com/500/500/nature/10',
+            'url' => 'https://lorempixel.com/1280/560/nature/10',
+            'thumbnail' => 'https://lorempixel.com/500/500/nature/10',
             'caption' => 'Photo 10'
         ],
         
          [
-            'url' => 'http://lorempixel.com/1280/560/transport/3',
-            'thumbnail' => 'http://lorempixel.com/500/500/transport/3',
+            'url' => 'https://lorempixel.com/1280/560/transport/3',
+            'thumbnail' => 'https://lorempixel.com/500/500/transport/3',
             'caption' => 'Photo 11'
         ],
         
          [
-            'url' => 'http://lorempixel.com/1280/560/transport/4',
-            'thumbnail' => 'http://lorempixel.com/500/500/transport/4',
+            'url' => 'https://lorempixel.com/1280/560/transport/4',
+            'thumbnail' => 'https://lorempixel.com/500/500/transport/4',
             'caption' => 'Photo 12'
         ],
         
         [
-            'url' => 'http://lorempixel.com/1280/560/transport/5',
-            'thumbnail' => 'http://lorempixel.com/500/500/transport/5',
+            'url' => 'https://lorempixel.com/1280/560/transport/5',
+            'thumbnail' => 'https://lorempixel.com/500/500/transport/5',
             'caption' => 'Photo 13'
         ],
         
